e48a8e1bf1b3f188814b69c343bc367f5e577af1
awathglagutu <adamharis393@gmail.com>
2026-07-26 18:49:57 +0800

    Added MainMenuFrame and SettingsDialog with GameSettings.java Settings is still not finished, need to move creation of settings object to somewhere consistent, which is from MainMenuFrame to main method

---
+++ src/spellingbee/GameSettings.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package spellingbee;
+
+/**
+ *
+ * @author adamh
+ */
+public class GameSettings {
+    private boolean soundEnabled = true;
+    private boolean endlessModeEnabled = true;
+    private boolean timerEnabled = true;
+    private boolean showHintEnabled = true;
+    private String theme = "Light";
+    
+    public GameSettings(){
+    }
+    
+    public boolean isSoundEnabled(){
+        return soundEnabled;
+    }
+    
+    public void setSoundEnabled(boolean soundEnabled){
+        this.soundEnabled = soundEnabled;
+    }
+    
+    public boolean isEndlessModeEnabled(){
+        return endlessModeEnabled;
+    }
+    
+    public void setEndlessModeEnabled(boolean endlessModeEnabled){
+        this.endlessModeEnabled = endlessModeEnabled;
+    }
+    
+    public boolean isTimerEnabled(){
+        return timerEnabled;
+    }
+    
+    public void setTimerEnabled(boolean timerEnabled){
+        this.timerEnabled = timerEnabled;
+    }
+    
+    public boolean isShowHintEnabled(){
+        return showHintEnabled;
+    }
+    
+    public void setShowHintEnabled(boolean showHintEnabled){
+        this.showHintEnabled = showHintEnabled;
+    }
+    
+    public String getTheme(){
+        return theme;
+    }
+    
+    public void setTheme(String theme){
+        this.theme = theme;
+    }
+    
+}
